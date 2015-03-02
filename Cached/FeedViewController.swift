9977b754a8d058a1c452b4c250773cb4bcc08d8c
Daniel Rakhamimov <danielrakh@gmail.com>
2015-03-02 02:28:37 -0500

    Remove Quick from pods, add as submodule, do someb basic test on VC

--- Cached/FeedViewController.swift
+++ Cached/FeedViewController.swift
@@ -8,17 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class FeedViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
---
+++ CachedTests/FeedViewControllerSpec.swift
@@ -0,0 +1,28 @@
+import Quick
+import Nimble
+import Cached
+import UIKit
+
+class FeedViewControllerSpec: QuickSpec {
+    override func spec() {
+        
+        var viewController:FeedViewController!
+        
+        beforeEach {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            viewController = storyboard.instantiateViewControllerWithIdentifier("FeedViewControllerID") as! FeedViewController
+        }
+        
+        describe(".viewDidLoad()") {
+            beforeEach {
+                let _ = viewController.view
+            }
+            
+            it("sets the navigation bar title to 'Cached' ", flags: [:]) {
+                expect(viewController.navigationItem.title).to(equal("Cached"))
+            }
+        }
+        
+
+    }
+}
