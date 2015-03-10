28b4b51bf5f9ee59a161eeb1fecc2f998f66730c
Daniel Rakhamimov <danielrakh@gmail.com>
2015-03-10 16:58:43 -0400

    Start Table View Cell programatically

--- Cached/CDFeedTableViewCell.swift
+++ Cached/CDFeedTableViewCell.swift
@@ -9,15 +9,23 @@
 import UIKit
 
 class CDFeedTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
     }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    
+    
     
     func setup() {
         backgroundColor = UIColor.clearColor()
